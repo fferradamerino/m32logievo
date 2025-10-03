b4a1a8612ada7b18b8d7a02b2349264eb7c67a77
Fabian Ferrada <fabian.ferrada2001@alumnos.ubiobio.cl>
2025-10-02 22:36:38 -0300

    Added release build script

---
+++ release.py
@@ -0,0 +1,14 @@
+# Este script se encarga de configurar el archivo M32.circ en el perfil "release".
+import xml.etree.ElementTree as ET
+
+debug_name = "jar#plugins/dist/M32Library.jar#M32Library"
+release_name = "jar#lib/M32Library.jar#M32Library"
+
+tree = ET.parse("M32.circ")
+root = tree.getroot()
+
+for child in root:
+    if child.tag == "lib" and child.attrib['desc'] == debug_name:
+        child.attrib['desc'] = release_name
+
+tree.write("M32 Release.circ")
