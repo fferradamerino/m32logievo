b834d3beebb69f70c7626cd68f9456df2eef8a86
Fabian Ferrada <fabian.ferrada2001@alumnos.ubiobio.cl>
2025-10-30 14:49:02 -0300

    Removed zero-branch in printf

--- tools/printf.s
+++ tools/printf.s
@@ -6,10 +6,10 @@
 printf:
     loop:
         ldub r4, [r2 + r0]      # Leer un byte de la string
-        be end                  # Si es 0 (fin de cadena), salir
         stb [r1 + r0], r4       # Escribir byte a la dirección TTY
         add r2, r2, 1           # Avanzar al siguiente carácter
         ba loop                 # Repetir el bucle
+        # Preguntar sobre el flag Z en la ALU !!!!
 
     end:
         nop                     # Fin de función
@@ -17,7 +17,7 @@ printf:
 main:
     add r1, r0, 8               # r1 = 0x8
     sll r1, r1, 24              # r1 = 0x08000000
-    add r2, r0, 0x30            # Las instrucciones aritméticas no tienen soporte
+    add r2, r0, 0x28            # Las instrucciones aritméticas no tienen soporte
                                 # de labels aún :p
     jmpl [r0 + printf], r3
 
