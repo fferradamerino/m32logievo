257c89bdb2bae5fa0fd3ec3499fdaaf9e9870f4e
Fabian Ferrada <fabian.ferrada2001@alumnos.ubiobio.cl>
2025-10-30 13:39:01 -0300

    Added printf (WIP)

---
+++ tools/printf.s
@@ -0,0 +1,27 @@
+.text
+    ba main                     # Salta a la función main del programa
+
+    # r1: dirección de la pantalla TTY
+    # r2: dirección de la string
+printf:
+        ldw r3, [r2 + r0]       # Cargar el primer byte de la string
+    loop:
+        ldub r4, [r2 + r0]      # Leer un byte de la string
+        be end                  # Si es 0 (fin de cadena), salir
+        stb [r1 + r0], r4       # Escribir byte a la dirección TTY
+        add r2, r2, 1           # Avanzar al siguiente carácter
+        ba loop                 # Repetir el bucle
+
+    end:
+        nop                     # Fin de función
+
+main:
+    add r1, r0, 8               # r1 = 0x8
+    sll r1, r1, 24              # r1 = 0x08000000
+    add r2, r0, 0x4             # Las instrucciones aritméticas no tienen soporte
+                                # de labels aún :p
+    jmpl [r0 + printf], r3
+
+.data
+    mensaje:
+        .ascii "Hola mundo"
