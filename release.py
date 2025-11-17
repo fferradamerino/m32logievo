f4d2fb3a1eb3339de7b1e34e84e8bee5ff2b34e6
fferradamerino <156674516+fferradamerino@users.noreply.github.com>
2025-11-17 13:44:55 -0300

    Fixed broken lib name

--- release.py
+++ release.py
@@ -1,8 +1,8 @@
 # Este script se encarga de configurar el archivo M32.circ en el perfil "release".
 import xml.etree.ElementTree as ET
 
-debug_name = "jar#plugins/dist/M32Library.jar#M32Library"
-release_name = "jar#lib/M32Library.jar#M32Library"
+debug_name = "jar#plugins/dist/M32Library.jar#m32plugin.M32Library"
+release_name = "jar#lib/M32Library.jar#m32plugin.M32Library"
 
 tree = ET.parse("M32.circ")
 root = tree.getroot()
