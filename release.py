2b7bc9120cd4bf94a65af0c900228b3c74a2cf11
fferradamerino <156674516+fferradamerino@users.noreply.github.com>
2026-05-05 01:38:33 -0400

    Fixing M32 path for Windows

--- release.py
+++ release.py
@@ -2,13 +2,14 @@
 import xml.etree.ElementTree as ET
 
 debug_name = "jar#plugins/dist/M32Library.jar#m32plugin.M32Library"
+debug_win_name = "jar#plugins\dist\M32Library.jar#m32plugin.M32Library"
 release_name = "jar#lib/M32Library.jar#m32plugin.M32Library"
 
 tree = ET.parse("M32.circ")
 root = tree.getroot()
 
 for child in root:
-    if child.tag == "lib" and child.attrib['desc'] == debug_name:
+    if child.tag == "lib" and (child.attrib['desc'] == debug_name or child.attrib['desc'] == debug_win_name):
         child.attrib['desc'] = release_name
 
 tree.write("M32 Release.circ")
